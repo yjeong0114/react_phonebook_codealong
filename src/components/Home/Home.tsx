ef8e406e3dd43d8c283426badd8b1dcb9b7b6ef4
Yoon Hwa Jeong <yjeong0114@hotmail.com>
2022-09-27 11:33:19 -0500

    first commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+//import { Navbar } from '../../components/Navbar'
+import { Link } from 'react-router-dom';
+
+interface Props {
+    title: string;
+}
+
+const useStyles = makeStyles({
+    background: {
+        backgroudImage: `linear-gradient(rgba(0,49,85) 0%, rgba(121,147,163,1) 47%, rgba(249,249,249,1) 100%)`,
+        width: '100%',
+        height: '100%',
+        backgroudPosition: 'center',
+        position: 'absolute',
+        zIndex: -1,    
+    },
+    main_text: {
+        textAlign: 'center',
+        position: 'relative',
+        top: '40%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        color: 'white',
+    },
+    button_text: {
+        color: 'white',
+        textDecoration: 'none',
+    },
+});
+
+export const Home = (props: Props) => {
+
+    const classes = useStyles();  
+
+    return (
+        <>
+            {/* <Navbar /> */}
+            <div className={`${classes.background}`}>
+                <div className={classes.main_text}>
+                    <h1>{props.title}</h1>
+                    <Button>
+                        <Link to ='/phonebook' className={classes.button_text}>Take me to my phonebook</Link>
+                    </Button>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './components';
+import reportWebVitals from './reportWebVitals';
+import './style.css';
+
+let myTitle = "Yoon's Phonebook"
+
+ReactDOM.render(
+  <React.StrictMode>
+
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <Home title= {myTitle} />
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
